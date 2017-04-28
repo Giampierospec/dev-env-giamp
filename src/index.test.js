bba55f19f15b8c15b158a2d9d7b1bcb93d88b2b5
Giampiero Specogna <giampi_12@hotmail.com>
2017-04-28 17:01:03 -0400

    Add appveyor

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,14 @@
 import {
   expect
 } from "chai";
+//Imports jsdom library
 import jsdom from "jsdom";
+//Imports the filesystem library
 import fs from "fs";
 
+//Testing
 describe("Our First test", function () {
+  //Arrow function declaration
   it("should pass", () => {
     expect(true).to.equal(true);
   });
@@ -17,6 +21,7 @@ describe("index.html", () => {
       const h1 = window.document.getElementsByTagName("h1")[0];
       expect(h1.innerHTML).to.equal("Hello World!");
       done();
+      //Closes the window is referring to.
       window.close();
     });
   });
